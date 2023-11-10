a3e751803b25f4e72b4d1e079ed2a927b7445b4d
Alexander Derevianko <alexander0derevianko@gmail.com>
2023-11-10 15:36:57 +0100

    Adding zsh highlight and completions

--- home/zsh/default.nix
+++ home/zsh/default.nix
@@ -2,21 +2,22 @@
   programs.zsh = {
     enable = true;
     enableCompletion = true;
-    #enableAutosuggestions = true;
+    enableAutosuggestions = true;
+    enableSyntaxHighlighting = true;
 
     oh-my-zsh = {
       enable = true;
 
       plugins = [ 
         "git" 
-        #"zsh-autosuggestions"
       ];
       theme = "bira";
     };    
 
     shellAliases = {
       ll = "exa -al";
-      #update = "sudo nixos-rebuild switch";
+      nixos-update = "sudo nixos-rebuild switch";
+      nixos-build = "sudo nixos-rebuild build";
     };
     #histSize = 10000;
     #histFile = "${config.xdg.dataHome}/zsh/history";
