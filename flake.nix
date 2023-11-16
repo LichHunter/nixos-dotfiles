6e3ec97938ac3eb6048b2ef01dc0f1d7477c2453
Alexander <alexander0derevianko@gmail.com>
2023-11-16 14:15:08 +0100

    Configured flakes+home-manager

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nixpkgs, home-manager, ... }: 
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config = {
+        allowUnfree = true;
+      };
+    };
+  in {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs system; };
+        modules = [ ./configuration.nix ];
+      };
+    };
+  };
+}
