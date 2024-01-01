ef24d2309121c233c35bf05ee3dd15c7daaad1c9
Alexander <alexander0derevianko@gmail.com>
2024-01-01 17:00:36 +0100

    Added calendar in bar

--- modules/waybar/waybar4.nix
+++ modules/waybar/waybar4.nix
@@ -41,6 +41,33 @@ in {
             "network"
             "battery"
         ];
+
+        "clock" = {
+            format = "{:%H:%M, %d, %B, %Y}  ";
+            format-alt = "{%R, :%A, %d %B, %Y}  ";
+            tooltip-format = "<tt><small>{calendar}</small></tt>";
+            calendar = {
+                mode = "year";
+                mode-mon-col = 3;
+                weeks-pos = "right";
+                on-scroll = 1;
+                on-click-right = "mode";
+                format = {
+                    months = "<span color='#ffead3'><b>{}</b></span>";
+                    days = "<span color='#ecc6d9'><b>{}</b></span>";
+                    weeks = "<span color='#99ffdd'><b>W{}</b></span>";
+                    weekdays = "<span color='#ffcc66'><b>{}</b></span>";
+                    today = "<span color='#ff6699'><b><u>{}</u></b></span>";
+                };
+            };
+            actions = {
+                on-click-right = "mode";
+                on-click-forward = "tz_up";
+                on-click-backward = "tz_down";
+                on-scroll-up = "shift_up";
+                on-scroll-down = "shift_down";
+            };
+        };
       };
     };
 
