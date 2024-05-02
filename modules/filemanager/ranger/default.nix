9cb9a4b8f0d7397bd7e9ccfcadf695a43f0db739
Alexander <alexander0derevianko@gmail.com>
2024-05-02 10:38:55 +0200

    Added ranger mount option

--- modules/filemanager/ranger/default.nix
+++ modules/filemanager/ranger/default.nix
@@ -4,4 +4,19 @@
   programs.ranger = {
     enable = true;
   };
+
+  xdg = {
+    enable = true;
+    configFile = {
+      "ranger/ranger_udisk_menu" = {
+        source = builtins.fetchGit {
+          url = "https://github.com/SL-RU/ranger_udisk_menu";
+          rev = "c892d447177051dd2fa97e2387b2d04bf8977de7";
+        };
+      };
+      "ranger/commands.py" = {
+        source = ./commands.py;
+      };
+    };
+  };
 }
