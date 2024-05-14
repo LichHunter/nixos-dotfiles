cafc4f9bd24612023cfe7b6c4d41938e10e70247
Alexander <alexander0derevianko@gmail.com>
2024-05-14 08:39:42 +0200

    Adding vmbox

--- machines/laptop/nixos-modules/virtmanager.nix
+++ machines/laptop/nixos-modules/virtmanager.nix
@@ -1,8 +1,44 @@
 { config, lib, pkgs, ... }:
 
 {
+  virtualisation = {
+    libvirtd = {
+      enable = true;
+      qemu = {
+        package = pkgs.qemu_kvm;
+        swtpm.enable = true;
+        ovmf.enable = true;
+        ovmf.packages = [ pkgs.OVMFFull.fd ];
+      };
+    };
+    spiceUSBRedirection.enable = true;
+  };
+
+  users.users.${config.variables.username}.extraGroups = [ "libvirtd" ];
+
+  environment.systemPackages = with pkgs; [
+    spice
+    spice-gtk
+    spice-protocol
+    virt-viewer
+    virtio-win
+    win-spice
+  ];
+  programs.virt-manager.enable = true;
+
+  home-manager.users.${config.variables.username} = {
+    dconf.settings = {
+      "org/virt-manager/virt-manager/connections" = {
+        autoconnect = [ "qemu:///system" ];
+        uris = [ "qemu:///system" ];
+      };
+    };
+  };
+
+
   virtualisation.virtualbox.host.enable = true;
-  users.extraGroups.vboxusers.members = [ "${config.variables.username}" ];
-  virtualisation.libvirtd.enable = true;
-  programs.dconf.enable = true; # virt-manager requires dconf to remember settings
+  users.extraGroups.vboxusers.members = [ "omen" ];
+  nixpkgs.config.allowUnfree = true;
+  virtualisation.virtualbox.host.enableExtensionPack = true;
+  virtualisation.virtualbox.guest.enable = true;
 }
