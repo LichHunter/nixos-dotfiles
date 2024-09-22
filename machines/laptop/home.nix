a9b853a2b9c01b48e34821a2575edc9fbf7ae9b9
Alexander Derevianko <alexander0derevianko@gmail.com>
2024-09-22 12:30:57 +0200

    Fixed zsh

--- machines/laptop/home.nix
+++ machines/laptop/home.nix
@@ -21,7 +21,25 @@ in {
   dov = {
     shell = {
       fish.enable = false;
-      zsh.enable = true;
+      zsh = {
+        enable = true;
+        shellAliases = {
+          ll = "exa -al";
+          nixos-build = "sudo nixos-rebuild build --show-trace --flake ~/nixos-dotfiles#laptop";
+          nixos-boot = "sudo nixos-rebuild boot --flake ~/nixos-dotfiles#laptop";
+          nixos-switch = "sudo nixos-rebuild switch --flake ~/nixos-dotfiles#laptop";
+          nixos-test = "sudo nixos-rebuild test --flake ~/nixos-dotfiles#laptop";
+          sc = "source $HOME/.zshrc";
+          doom = "$HOME/.config/emacs/bin/doom";
+          vim = "nvim";
+          java21 = "export JAVA_HOME='/home/omen/jdk/openjdk21' && mvn -v";
+          java17 = "export JAVA_HOME='/home/omen/jdk/openjdk17' && mvn -v";
+          java11 = "export JAVA_HOME='/home/omen/jdk/openjdk11' && mvn -v";
+          sudo = "doas";
+          psax = "ps ax | grep";
+          cp = "rsync -ah --progress";
+        };
+      };
     };
     browser = {
       brave.enable = false;
