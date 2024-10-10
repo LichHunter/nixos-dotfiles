1ff7ffcb2624741ef34c495b99307dbfc7c74456
uzume <uzume@uzume.com>
2024-10-10 20:08:08 +0200

    Uzume sops fixes

--- machines/uzume/home.nix
+++ machines/uzume/home.nix
@@ -2,7 +2,7 @@
 
 {
   imports = [
-    inputs.sops-nix.homManagerModules.sops
+    inputs.sops-nix.homeManagerModules.sops
   ];
 
   home = {
@@ -42,7 +42,7 @@
     validateSopsFiles = false;
 
     age = {
-      sshKeyPath = [ "/etc/ssh/ssh_host_ed25519_key" ];
+      sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
       keyFile = "/var/lib/sops-nix/key.txt";
       generateKey = true;
     };
--- machines/uzume/sops.nix
+++ machines/uzume/sops.nix
@@ -10,7 +10,7 @@
     validateSopsFiles = false;
 
     age = {
-      sshKeyPath = [ "/etc/ssh/ssh_host_ed25519_key" ];
+      sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
       keyFile = "/var/lib/sops-nix/key.txt";
       generateKey = true;
     };
